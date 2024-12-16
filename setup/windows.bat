c994acbcc94f6cb4c1aaa8c99279af61982cf839
MiuJoan6952_2 <117433468+miujoan6952@users.noreply.github.com>
2024-12-16 19:53:42 +0700

    reset to og

--- setup/windows.bat
+++ setup/windows.bat
@@ -1,15 +1,14 @@
 @echo off
 color 0a
 cd ..
-@echo on
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
-haxelib install flixel 5.8.0
+haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
-haxelib install hscript-iris
+haxelib install hscript-iris 1.0.2
 rem for debuging
 haxelib install hscript
 haxelib install tjson
